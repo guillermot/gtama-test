47a2304d7414c3a5727e09385975bfaee3fcc446
guillermot <guillermo.tamanaha@gmail.com>
2017-07-21 22:37:43 -0300

    offline

--- workbox-cli-config.js
+++ workbox-cli-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   "globDirectory": ".\\",
   "globPatterns": [
-    "**/*.{html,json,css}"
+    "**/*.{sample,idx,pack,html,json,css}"
   ],
   "swDest": "sw.js",
   "globIgnores": [
